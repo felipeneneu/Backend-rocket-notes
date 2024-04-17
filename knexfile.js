2d9a1baf525cea88c45d628a84352c2098993bed
felipeneneu <feliperpg3@gmail.com>
2024-04-17 19:23:55 -0300

    migrate criado com knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,5 @@
-const path = require("path")
+const path = require("path");
+
 
 module.exports = {
 
@@ -6,6 +7,9 @@ module.exports = {
     client: 'sqlite3',
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db")
+    },
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
     },
     useNullAsDefault: true
   },
