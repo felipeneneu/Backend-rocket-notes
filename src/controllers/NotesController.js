149370ddddaf90dedaa480d71ad2091990062639
felipeneneu <feliperpg3@gmail.com>
2024-04-18 00:56:02 -0300

    Show notes, route get

---
+++ src/controllers/NotesController.js
@@ -0,0 +1,50 @@
+const knex = require("../database/knex")
+
+class NotesController {
+    async create(request, response) {
+        const { title, descriptions, tags, links } = request.body
+        const { user_id } = request.params
+
+
+        const [note_id] = await knex("notes").insert({
+            title,
+            descriptions,
+            user_id
+        })
+
+        const linksIsert = links.map(link => {
+            return {
+                note_id,
+                url: link
+            }
+        })
+        await knex("links").insert(linksIsert)
+
+
+        const tagsIsert = tags.map(name => {
+            return {
+                note_id,
+                name,
+                user_id
+            }
+        })
+        await knex("tags").insert(tagsIsert)
+
+        response.json()
+    }
+
+    async show(request, response) {
+        const { id } = request.params
+        const note = await knex("notes").where({ id }).first()
+        const tags = await knex("tags").where({ note_id: id }).orderBy("name")
+        const links = await knex("links").where({ note_id: id }).orderBy("created_at")
+
+        return response.json({
+            ...note,
+            tags,
+            links
+        })
+    }
+}
+
+module.exports = NotesController
