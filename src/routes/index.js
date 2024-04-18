8172cb017aede2b9f818c04997a09bbcabfe9abb
felipeneneu <feliperpg3@gmail.com>
2024-04-18 19:25:19 -0300

    Sessions Controller

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 const { Router } = require("express")
 
 const userRouter = require("./users.routes")
+const sessionsRouter = require("./sessions.routes")
 const notesRouter = require("./notes.routes")
 const tagsRouter = require("./tags.routes")
 
 const routes = Router()
 
+routes.use("/sessions", sessionsRouter)
 routes.use("/users", userRouter)
 routes.use("/notes", notesRouter)
 routes.use("/tags", tagsRouter)
